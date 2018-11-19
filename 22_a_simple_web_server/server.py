a4497a0f85c0482eda672197b031b028438a12f7
Jing Lin <moderatelyfunctionalprogrammer@gmail>
2018-11-18 22:26:45 -0500

    committing simple web server

--- 22_a_simple_web_server/server.py
+++ 22_a_simple_web_server/server.py
@@ -14,23 +14,38 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
 	Page = '''\
 		<html>
 			<body>
-				<p>Hello, web!</p>
+				<table>
+					<tr> <td>Header</td>      <td>Value</td>          </tr>
+					<tr> <td>Date</td>        <td>{date_time}</td>    </tr>
+					<tr> <td>Client Host</td> <td>{client_host}</td>  </tr>
+					<tr> <td>Client Port</td> <td>{client_port}s</td> </tr>
+					<tr> <td>Command</td>     <td>{command}</td>      </tr>
+					<tr> <td>Path</td>        <td>{path}</td>         </tr>
+				</table>
 			</body>
 		</html>
 	'''
 
-	# def create_page(self):
-	# 	pass
+	def create_page(self):
+		values = {
+			'date_time'   : self.date_time_string(),
+			'client_host' : self.client_address[0],
+			'client_port' : self.client_address[1],
+			'command'     : self.command,
+			'path'        : self.path
+		}
 
-	# def send_page(self, page):
-
-	# Handles a GET request
-	def do_GET(self):
+	def send_page(self, page):
 		self.send_response(200)
 		self.send_header("Content-Type", "text/html")
 		self.send_header("Content-Length", str(len(self.Page)))
 		self.end_headers()
 		self.wfile.write(self.Page.encode('utf-8')) # Strings in Python3 are Unicode so have to convert to binary
+
+	# Handles a GET request
+	def do_GET(self):
+		page = self.create_page()
+		self.send_page(page)
 
 if __name__ == '__main__':
 	serverAddress = ('', 8080) # run on the current machine with port 8080
