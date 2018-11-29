c651d6df25d3ff9587849294cae99dc2bc48828f
Jing Lin <moderatelyfunctionalprogrammer@gmail.com>
2018-11-28 19:05:02 -0500

    5 instruction interpreter

--- 13_python_interpreter/interpreter.py
+++ 13_python_interpreter/interpreter.py
@@ -2,6 +2,7 @@ class Interpreter:
 
 	def __init__(self):
 		self.stack = []
+		self.environment = {}
 
 	def LOAD_VALUE(self, number):
 		self.stack.append(number)
@@ -16,21 +17,45 @@ class Interpreter:
 		result = first_num + second_num
 		self.stack.append(result)
 
+	def STORE_NAME(self, name):
+		value = self.stack.pop()
+		self.environment[name] = value
+
+	def LOAD_NAME(self, name):
+		value = self.environment[name]
+		self.stack.append(value)
+
+	def parse_argument(self, instruction, argument, what_to_execute):
+		''' Understand what the argument to each instruction means.'''
+		numbers = ['LOAD_VALUE']
+		names = ['LOAD_NAME', 'STORE_NAME']
+
+		if instruction in numbers:
+			argument = what_to_execute['numbers'][argument]
+		elif instruction in names:
+			argument = what_to_execute['names'][argument]
+
+		return argument
+
 	def run_code(self, what_to_execute):
 		instructions = what_to_execute['instructions']
 		numbers = what_to_execute['numbers']
 		for each_step in instructions:
-			instruction, argument = each_step
+			instruction, argument = each_step # argument is an instruction specific index
+			argument = self.parse_argument(instruction, argument, what_to_execute) # argument is now an instruction parameter
+
 			if instruction == 'LOAD_VALUE':
-				number = numbers[argument]
-				self.LOAD_VALUE(number)
+				self.LOAD_VALUE(argument)
 			elif instruction == 'ADD_TWO_VALUES':
 				self.ADD_TWO_VALUES()
 			elif instruction == 'PRINT_ANSWER':
 				self.PRINT_ANSWER()
+			elif instruction == 'STORE_NAME':
+				self.STORE_NAME(argument)
+			elif instruction == 'LOAD_NAME':
+				self.LOAD_NAME(argument)
 
-
-what_to_execute = {
+two_sum_executions = {
 	'instructions': [('LOAD_VALUE', 0),
 					 ('LOAD_VALUE', 1),
 					 ('ADD_TWO_VALUES', None),
@@ -38,5 +63,43 @@ what_to_execute = {
 	'numbers': [7, 5]
 }
 
+three_sum_executions = {
+	'instructions': [('LOAD_VALUE', 0),
+					 ('LOAD_VALUE', 1),
+					 ('ADD_TWO_VALUES', None),
+					 ('LOAD_VALUE', 2),
+					 ('ADD_TWO_VALUES', None),
+					 ('PRINT_ANSWER', None)],
+	'numbers': [7, 5, 8]
+}
+
+'''
+	a = 1
+	b = 2
+	print(a + b)
+	Python code for bytecode below
+
+'''
+variable_executions = {
+	'instructions': [('LOAD_VALUE', 0),
+					 ('STORE_NAME', 0),
+					 ('LOAD_VALUE', 1),
+					 ('STORE_NAME', 1),
+					 ('LOAD_NAME', 0),
+					 ('LOAD_NAME', 1),
+					 ('ADD_TWO_VALUES', None),
+					 ('PRINT_ANSWER', None)],
+	'numbers': [1, 2],
+	'names': ['a', 'b']
+}
+
 interpreter = Interpreter()
-interpreter.run_code(what_to_execute)
+interpreter.run_code(two_sum_executions)
+interpreter.run_code(three_sum_executions)
+interpreter.run_code(variable_executions)
+
+
+
+
+
+
